c7ecf89bfa6eeeee6199693b9807283fdbfaf1c6
unknown <Grinya201@yandex.ru>
2022-06-07 15:24:24 +0300

    fixed space

--- FirstTest.java
+++ FirstTest.java
@@ -10,29 +10,21 @@ public class FirstTest extends WebDriverSet {
 
     @Test
     public void firstTest1() {
-
         testdriver.get("https://www.delivery-club.ru/");
-
         String title = testdriver.getTitle();
         Assert.assertTrue(title.equals("Delivery Club — Доставка еды и продуктов"));
         testdriver.findElement(By.className("header-login-button")).click();
         testdriver.findElement(By.className("login-popup__legal-notes")).click();
-
     }
 
     @Test
     public void firstTest2() {
         testdriver.get("https://raftds.ru/");
-
         String title = testdriver.getTitle();
         Assert.assertFalse(title.equals("RAAAAAAAAAAAAAAAAAAAAAFT"));
         testdriver.findElement(By.cssSelector(".MuiGrid-root .MuiList-root li button")).click();
     }
 
-
-
-
-
-    }
+}
 
 
